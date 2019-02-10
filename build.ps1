098bf21ace82ca1d093054acf5fc7d201b2e58d8
cmarttest2 <47405316+cmarttest2@users.noreply.github.com>
2019-02-09 16:31:31 -0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,9 @@
 ### Build script called by Azure Pipelines
 Write-Host "I'm the build script"
+
+# Install Pester module if it is not already installed
+if (-not (Get-Module -Name Pester -ListAvailable))
+{
+    Write-Host "Installing Pester"
+    Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false
+}
