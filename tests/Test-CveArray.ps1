a755648045f4a3d601f013c7f6d74150c04b7dc9
cmarttest2 <47405316+cmarttest2@users.noreply.github.com>
2019-02-10 08:11:19 -0800

    Create Test-CveArray.ps1

---
+++ tests/Test-CveArray.ps1
@@ -0,0 +1,21 @@
+Describe "Add-Numbers" {
+    It "adds positive numbers" {
+        $sum = Add-Numbers 2 3
+        $sum | Should Be 5
+    }
+
+    It "adds negative numbers" {
+        $sum = Add-Numbers (-2) (-2)
+        $sum | Should Be (-4)
+    }
+
+    It "adds one negative number to positive number" {
+        $sum = Add-Numbers (-2) 2
+        $sum | Should Be 0
+    }
+
+    It "concatenates strings if given strings" {
+        $sum = Add-Numbers two three
+        $sum | Should Be "twothree"
+    }
+}
