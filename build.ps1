79f44269f51cf05bbf400620c82bdda87097f4cb
cmarttest2 <47405316+cmarttest2@users.noreply.github.com>
2019-02-10 08:03:53 -0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -7,3 +7,30 @@ if (-not (Get-Module -Name Pester -ListAvailable))
     Write-Host "Installing Pester"
     Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false
 }
+
+function Add-Numbers($a, $b) {
+    
+    return $a + $b
+}
+
+Describe "Add-Numbers" {
+    It "adds positive numbers" {
+        $sum = Add-Numbers 2 3
+        $sum | Should Be 5
+    }
+
+    It "adds negative numbers" {
+        $sum = Add-Numbers (-2) (-2)
+        $sum | Should Be (-4)
+    }
+
+    It "adds one negative number to positive number" {
+        $sum = Add-Numbers (-2) 2
+        $sum | Should Be 0
+    }
+
+    It "concatenates strings if given strings" {
+        $sum = Add-Numbers two three
+        $sum | Should Be "twothree"
+    }
+}
