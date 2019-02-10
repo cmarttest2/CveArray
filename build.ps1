9ed4b1def2f334724e4646e2ce17d26e3cf351ad
cmarttest2 <47405316+cmarttest2@users.noreply.github.com>
2019-02-10 09:04:31 -0800

    try using LogIssue to craft a better build message on test fail

--- build.ps1
+++ build.ps1
@@ -19,11 +19,14 @@ function Add-Numbers($a, $b) {
 $TestResults = Invoke-Pester -Script .\tests -PassThru
 
 #$TestResults
-Write-Host "$("##vso[task.LogIssue type=error;]") $("the task.LogIssue Azure Pipelines logging command reported that") $('foo')"
-Write-Host "$("##vso[task.setvariable variable=ErrorMessage]") $('foo')"
+#Write-Host "$("##vso[task.LogIssue type=error;]") $("the task.LogIssue Azure Pipelines logging command reported that") $('foo')"
+#Write-Host "$("##vso[task.setvariable variable=ErrorMessage]") $('foo')"
+$testResults.TestResult | Where Result -EQ Failed
 
+<#
 if($TestResults.FailedCount -gt 0)
 {
     Write-Error -Message ($testResults.TestResult | Where Result -EQ Failed)
     #exit 1
 }
+#>
